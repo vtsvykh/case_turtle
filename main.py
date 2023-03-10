b9aefd069690bdca490f2a5fb195291ec4689a06
vtsvykh <v.tsvykh0@gmail.com>
2023-03-10 20:05:31 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+'''
+"Circus"
+Group:
+Fishchukova Sofia
+Tsvykh Viktoria
+'''
+
+import turtle
+
+def triangle(x, y, a, b, angle, color, fill):
+    '''
+    Function for draw triangle.
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param angle:
+    :param color:
+    :param fill:
+    :return:
+    '''
+
+    pass
+
+def ellipse(x, y, a, b, color, fill, pensize):
+    '''
+    Function for draw ellipse.
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param color:
+    :param fill:
+    :param pensize:
+    :return:
+    '''
+
+    pass
+
+def parallelogram(x, y, a, b, angle_1, angle_2, color, fill):
+    '''
+    Function for draw parallelogram.
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param angle_1:
+    :param angle_2:
+    :param color:
+    :param fill:
+    :return:
+    '''
+
+    pass
+
+def draw_fox():
+    '''Drawing fox on big ball.'''
+
+    pass
+
+def draw_clown():
+    '''Drawing clown with balls.'''
+
+    pass
+
+def main():
+    '''Main function.'''
+
+    draw_fox()
+    draw_clown()
+
+if __name__ == '__main__':
+    main()
