43faaac9ba4a785be9a1c7730fec0e007e7d212e
fishukaa <103937450+fishuks@users.noreply.github.com>
2023-03-12 19:52:30 +0700

    changed the clown function, added a background function

--- main.py
+++ main.py
@@ -91,6 +91,10 @@ def parallelogram(x, y, a, b, angle_1, angle_2, color, fill):
 
     turtle.end_fill()
 
+def back_groung():
+    turtle.bgcolor('navajowhite2')
+    ellipse(-570,-400,800,100,'chocolate4','orangered3',70)
+
 
 def draw_fox():
     '''Drawing fox on big ball.'''
@@ -131,43 +135,44 @@ def draw_fox():
 def draw_clown():
     '''Drawing clown with balls.'''
 
-    ellipse(-200, -110, 25, 60, 'maroon', 'maroon4', 5)
+    ellipse(-200, -110, 25, 60, 'maroon', 'maroon4', 1)
     turtle.seth(0)
-    ellipse(-130, -50, 35, 2, 'peachpuff', 'peachpuff', 5)
+    ellipse(-130, -50, 35, 2, 'black', 'peachpuff', 1)
     turtle.seth(0)
-    ellipse(-280, -53, 35, 2, 'peachpuff', 'peachpuff', 5)
+    ellipse(-280, -53, 35, 2, 'black', 'peachpuff', 1)
     turtle.seth(-45)
     triangle(-172, -20, 70, 40, 100, 197, 'cornflowerblue', 'cornflowerblue')
     triangle(-192, -20, 70, 40, 100, 0, 'cornflowerblue', 'cornflowerblue')
     turtle.seth(180)
     triangle(-163, -20, 35, 35, 120, 0, 'mediumorchid4', 'mediumorchid4')
-    ellipse(-270, -167, 45, 2, 'darkgrey', 'darkgrey', 5)
+    ellipse(-270, -167, 45, 2, 'darkgrey', 'darkgrey', 1)
     turtle.seth(0)
-    ellipse(-163, -167, 45, 2, 'darkgrey', 'darkgrey', 5)
+    ellipse(-163, -167, 45, 2, 'darkgrey', 'darkgrey', 1)
     turtle.seth(275)
     triangle(-174, -95, 70, 40, 100, 235, 'cornflowerblue', 'cornflowerblue')
     triangle(-195, -95, 70, 40, 100, 0, 'cornflowerblue', 'cornflowerblue')
-    ellipse(-200, -15, 28, 28, 'peachpuff', 'peachpuff', 5)
-    ellipse(-182, -4, 5, 5, 'red3', 'red3', 5)
-    ellipse(-173, 10, 5, 5, 'black', 'white', 5)
-    ellipse(-170, 13, 2, 2, 'black', 'black', 5)
-    ellipse(-193, 10, 5, 5, 'black', 'white', 5)
-    ellipse(-191, 13, 2, 2, 'black', 'black', 5)
+    ellipse(-200, -15, 28, 28, 'black', 'peachpuff', 1)
+    ellipse(-184, -4, 5, 5, 'red3', 'red3', 1)
+    ellipse(-170, 8, 5, 5, 'black', 'white', 1)
+    ellipse(-170, 13, 2, 2, 'black', 'black', 1)
+    ellipse(-196, 8, 5, 5, 'black', 'white', 1)
+    ellipse(-196, 13, 2, 2, 'black', 'black', 1)
     turtle.seth(360)
     triangle(-210, 22, 60, 60, 120, 0, 'maroon', 'maroon4')
-    ellipse(-183, 73, 5, 5, 'cornflowerblue', 'cornflowerblue', 5)
+    ellipse(-183, 73, 5, 5, 'cornflowerblue', 'cornflowerblue', 1)
     parallelogram(-179, 50, 7, 7, 90, 90, 'black', 'yellow')
     triangle(-195, 40, 7, 8, 120, 0, 'black', 'orangered')
     parallelogram(-175, 30, 7, 7, 90, 90, 'black', 'moccasin')
-    ellipse(-90, -10, 25, 25, 'black', 'yellow', 5)
-    ellipse(-130, 80, 25, 25, 'black', 'green', 5)
-    ellipse(-190, 160, 25, 25, 'black', 'blue', 5)
-    ellipse(-250, 100, 25, 25, 'black', 'red', 5)
-    ellipse(-280, 20, 25, 25, 'black', 'orange', 5)
+    ellipse(-90, -10, 25, 25, 'black', 'yellow', 1)
+    ellipse(-130, 80, 25, 25, 'black', 'green', 1)
+    ellipse(-190, 160, 25, 25, 'black', 'blue', 1)
+    ellipse(-250, 100, 25, 25, 'black', 'red', 1)
+    ellipse(-280, 20, 25, 25, 'black', 'orange', 1)
 
 
 def main():
     '''Main function.'''
+    back_groung()
     draw_clown()
     draw_fox()
 
